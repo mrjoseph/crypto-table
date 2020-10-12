8d2b36e3293b2ee6448b190438978bd8ee980295
Trevor Joseph <trevor.joseph@river-island.com>
2020-10-12 08:49:02 +0100

    updated unit test for App

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { render, screen, act } from "@testing-library/react";
+import { render, screen, act, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 
 import App from "./App";
@@ -33,6 +33,26 @@ describe("App", () => {
     it("should render a table", async () => {
       await act(async () => render(<App />));
       expect(screen.getByText("Something went wrong")).toBeInTheDocument();
+    });
+  });
+
+  describe("Currency select", () => {
+    it("should update the currency select state", async () => {
+      fetch.mockReturnValueOnce({
+        ok: true,
+        json: jest.fn(() => mockReturnValueOnce),
+        headers: {
+          get: () => "application/json;",
+        },
+      });
+      await act(async () => render(<App />));
+      expect(screen.getByText("USD")).toBeInTheDocument();
+      const select = screen.getByTestId("select-currency");
+      fireEvent.change(select, {
+        target: { value: "GBP" },
+      });
+
+      expect(screen.getByText("GBP")).toBeInTheDocument();
     });
   });
 });
